ae3d638ca4aa266e8d7069d65e295fae9398bdb7
valar <68338647+76616c6172@users.noreply.github.com>
2024-01-25 11:28:38 -0800

    ci: add mypy

--- plox.py
+++ plox.py
@@ -6,61 +6,52 @@ hadError: bool = False
 
 # ******************** error handling ********************
 
-
 def report(line: int, where, msg: str) -> None:
-    print(f"[line {line}] Error {where}: {msg}")
-
-    global hadError
-    hadError = True
-
+	print(f"[line {line}] Error {where}: {msg}")
+	global hadError
+	hadError = True
 
 def error(line: int, msg: str) -> None:
-    report(line, "", msg)
-
+	report(line, "", msg)
 
 # ******************** core interpreter functions ********************
 
-
 def run(src: str) -> None:
-    # "scan" tokens here
-    tokens: list[str] = src.split()
+	# "scan" tokens here
+	tokens: list[str] = src.split()
 
-    # For now just print the tokens.
-    for token in tokens:
-        print(token)
-
+	# For now just print the tokens.
+	for token in tokens:
+		print(token)
 
 # ******************** entrypoint ********************
 
-
 def runFile(path: str) -> None:
-    file_content = pathlib.Path(path).read_text()
-    run(file_content)
-    sys.exit(64) if hadError else None
-
+	file_content = pathlib.Path(path).read_text()
+	run(file_content)
+	sys.exit(64) if hadError else None
 
 def runPrompt() -> None:
-    global hadError
+	global hadError
 
-    while True:
-        try:
-            line = input("> ")
-            run(line)
-            hadError = False
-        except EOFError:
-            print()
-            break
-
+	while True:
+		try:
+			line = input("> ")
+			run(line)
+			hadError = False
+		except EOFError:
+			print()
+			break
 
 def main(args: List[str]) -> None:
-    if len(args) > 1:
-        print("Usage: plox [script]")
-        sys.exit(64)
-    elif len(args) == 1:
-        runFile(args[0])
-    else:
-        runPrompt()
-
+	if len(args) > 1:
+		print("Usage: plox [script]")
+		sys.exit(64)
+	elif len(args) == 1:
+		runFile(args[0])
+	else:
+		runPrompt()
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+	main(sys.argv[1:])
+
